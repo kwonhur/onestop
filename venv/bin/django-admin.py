401b95cf3d212ee25d2a2d0283bb360a98e72b5b
Kwon Hur <kwonhur1020@gmail.com>
2020-04-16 10:33:27 -0400

    Save before ejecting

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kwon/Downloads/inspiniaadmin-292/Bootstrap 3.3/ReactJS_Seed_Project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
